07ea239d856b6b635b9ac596a27bc42530ffe95a
Christian Brida <bridachristian@gmail.com>
2024-01-25 22:59:00 +0100

    Delete remsen_landsat7/cfg.py

--- remsen_landsat7/cfg.py
+++
@@ -1,5 +0,0 @@
-"""This configuration module is a container for parameters and constants."""
-import os
-
-datadir = 'C:/Users/Christian/OneDrive/Desktop/Family/Christian/MasterMeteoUnitn/Corsi/3_terzo_semestre/AtmosphericRadiationRemoteSensing/SPBL7.tif'
-
